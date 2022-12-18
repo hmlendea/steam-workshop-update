73496682edb35e50f2cdd09127b87a59505dec8d
Horațiu Mlendea <Horatiu.Mlendea@ProtonMail.com>
2022-12-18 10:46:18 +0200

    Uploaded the code

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# steam-workshop-upload
-GitHub Action to update a mod to the Steam Workshop
+# About
+
+Takes inspiration from:
+ - [workshop-upload](https://github.com/arma-actions/workshop-upload)
+ - [steam-workshop-upload](https://github.com/Weilbyte/steam-workshop-upload)
+
+# Usage
+
+```yaml
+- name: steam-workshop-upload
+  uses: hmlendea/steam-workshop-upload@v1
+  with:
+    appid: 1158310 # The game's Steam App ID
+    itemid: 0000000000 # The mod's Steam Workshop Item ID
+    path: 'dist' # Relative path to your mod's folder, starting from the repository root
+  env:
+    STEAM_USERNAME: ${{ secrets.STEAM_USERNAME }} # The Steam account username
+    STEAM_PASSWORD: ${{ secrets.STEAM_PASSWORD }} # The Steam account password
+    STEAM_2FASEED: ${{ secrets.STEAM_2FASEED }} # The Steam account 2FA seed (standard TOTP, not the SteamGuard base64 secret)
+```
---
+++ entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [[ -z "${STEAM_USERNAME}" ]]; then
+  echo "FATAL: Environment variable STEAM_USERNAME is required but not provided."
+  exit 1
+fi
+
+if [[ -z "${STEAM_PASSWORD}" ]]; then
+  echo "FATAL: Environment variable STEAM_PASSWORD is required but not provided."
+  exit 1
+fi
+
+cat << EOF > /home/steam/workshop.vdf
+"workshopitem"
+{
+  "appid" "${INPUT_APPID}"
+  "contentfolder" "${GITHUB_WORKSPACE}/${INPUT_PATH}"
+  "changenote" "${INPUT_CHANGENOTE}"
+  "publishedfileid" "${INPUT_ITEMID}"
+}
+EOF
+
+echo "$(cat /home/steam/workshop.vdf)"
+
+if [[ -z "${STEAM_2FASEED}" ]]; then
+  /home/steam/steamcmd/steamcmd.sh +@ShutdownOnFailedCommand 1 +login ${STEAM_USERNAME} ${STEAM_PASSWORD} +workshop_build_item /home/steam/workshop.vdf +quit
+else
+  export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+  STEAM_2FACODE=$(/home/steam/steam-totp ${STEAM_2FASEED})
+  if [[ $? -ne 0 ]]; then
+    exit 1
+  fi
+  /home/steam/steamcmd/steamcmd.sh +@ShutdownOnFailedCommand 1 +login ${STEAM_USERNAME} ${STEAM_PASSWORD} ${STEAM_2FACODE} +workshop_build_item /home/steam/workshop.vdf +quit
+fi
+
+[ $? -eq 0 ] && exit 0 || (
+    echo stderr
+    echo "$(cat /home/steam/Steam/logs/stderr.txt)"
+    echo
+    echo workshop_log
+    echo "$(cat /home/steam/Steam/logs/workshop_log.txt)"
+
+    exit 1
+)
